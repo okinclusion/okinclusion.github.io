30570d21d4da5867cd47505ed33651aa0cf2dc77
Sierra Williams <78832175+sierrawillliams@users.noreply.github.com>
2024-01-20 12:55:49 -0800

    changing to react

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+import ContactMain from '../ContactMain';
+import ContactForm from '../ContactForm';
+import Footer from '../Footer';
+
+function Contact () {
+    return (
+        <>
+            <ContactMain/>
+            <ContactForm/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Contact;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App.css';
+import Main from '../Main';
+import Footer from '../Footer';
+
+function Home () {
+    return (
+        <>
+            <Main/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Home;
