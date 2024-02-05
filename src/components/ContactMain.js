7d960d91602a713b1d3ab3a30dbc2254b93e9958
Sierra Williams <williamssierra90@gmail.com>
2024-02-04 18:26:12 -0800

    fixing contact page

--- src/components/ContactMain.js
+++ src/components/ContactMain.js
@@ -8,11 +8,8 @@ import './Main.css';
 function ContactMain() {
   return (
     <div className='main-container'>
-        <video autoPlay loop muted playsInline>
-            <source src={backgroundVideo} type='video/mp4' alt='Video by PCh Art from Pixabay'/>
-        </video>
         <div className='responsive-type-animation'>
-          <TypeAnimation className='name' sequence={["Contact", 1000]} speed={20} style={{ fontSize: '12vw', fontFamily: 'Tahoma', textShadow: '3px 2px #5b05f9', color: '#fff'}}/>
+          <TypeAnimation className='name' sequence={["Contact", 1000]} speed={20} style={{ fontSize: '8vw', fontFamily: 'Tahoma', textShadow: '3px 2px #5b05f9', color: '#fff'}}/>
         </div> 
     </div>
   )
