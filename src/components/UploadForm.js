186d3f90c5e7676be9e4757360ccd25c284e7c81
Sierra Williams <78832175+sierrawillliams@users.noreply.github.com>
2024-06-10 16:22:07 -0700

    added apply options

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,39 +1,45 @@
-import React, { useState } from "react";
-import emailjs from "emailjs-com";   
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function UploadForm() {
-  const [file, setFile] = useState(null);
+    const form = useRef();
 
-  emailjs.sendForm('service_vvxcblm', 'template_ijg3p4h').then(
-    (response) => {
-      console.log('SUCCESS!', response.status, response.text);
-    },
-    (error) => {
-      console.log('FAILED...', error);
-    },
-  );
+    const sendEmail = (e) => {
+    e.preventDefault();
+    // service_id, templte_id and public key will get from Emailjs website when you create account and add template service and email service 
+    emailjs.sendForm('service_vvxcblm', 'template_ijg3p4h', form.current, 
+    'TInXkn9D4lftKds5g')
+      .then((result) => {
+          console.log(result.text);
+          alert('Application Sent!')
+      }, (error) => {
+          console.log(error.text);
+          alert('Error');
+      });
+    };
 
   return (
-    <form id="form">
-        <div class="field">
-            <label for="from_name">from_name</label>
-            <input type="text" name="from_name" id="from_name"/>
+    <div className='container'>
+        <div className='contact-container'>
+            <h1 className='messageTitle'>Send Us Your Resume</h1>
+            <form ref={form} onSubmit={sendEmail}>
+                <label className='name'>Name:</label>
+                <br/>
+                <input type="text" name="user_name" />
+                <br/>
+                <br/>
+                <label className='email'>Email:</label>
+                <br/>
+                <input type="email" name="user_email" />
+                <br/>
+                <br/>
+                <input className='file' type="file" name="file" id="file"/>
+                <br/>
+                <br/>
+                <input className='btn-submit' type="submit" value="Send" />
+            </form>
         </div>
-        <div class="field">
-            <label for="to_name">to_name</label>
-            <input type="text" name="to_name" id="to_name"/>
-        </div>
-        <div class="field">
-            <label for="reply_to">reply_to</label>
-            <input type="text" name="reply_to" id="reply_to"/>
-        </div>
-        <div class="field">
-            <label for="file">file</label>
-            <input type="file" name="file" id="file"/>
-        </div>
-
-        <input type="submit" id="button" value="Send Email" />
-    </form>
+    </div>
     );
 };
 
