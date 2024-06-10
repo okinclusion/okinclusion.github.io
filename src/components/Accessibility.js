03fab00e49c2fb09f7b34c41030afc6ed3233d6b
Sierra Williams <williamssierra90@gmail.com>
2024-06-10 14:38:18 -0700

    Update Accessibility.js

--- src/components/Accessibility.js
+++ src/components/Accessibility.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import { React, useState } from 'react';
 import '../App.css';
 import { Button } from './Button';
 import { Button2 } from './Button2';
 
 function Accessibility() {
+    const [fontSize, setFontSize] = useState(14);
     return (
         <div className='main-container'>
             <div className='main-btns'>
-                <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>Aa</Button>
-                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--small'>Aa</Button>
+                <Button onChange={() => setFontSize(fontSize + 1)} className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>Aa</Button>
+                <Button onChange={() => setFontSize(fontSize - 1)} className='btns' buttonStyle='btn--primary' buttonSize='btn--small'>Aa</Button>
             </div>
         </div>
     )
