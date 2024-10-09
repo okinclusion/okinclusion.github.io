cd2049537a241a844040d84f4f492cabeb1602ad
Sierra Williams <78832175+sierrawillliams@users.noreply.github.com>
2024-10-09 12:29:20 -0700

    Update HomeShareApply.js

--- src/components/HomeShareApply.js
+++ src/components/HomeShareApply.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { TypeAnimation } from 'react-type-animation';       
-import backgroundVideo from '../videos/snow.mp4';
+import { TypeAnimation } from 'react-type-animation';   
 import Footer from './Footer';
 import HomeShareForm from './HomeShareForm';
 
@@ -9,9 +8,6 @@ function Apply() {
   return (
     <>
         <div className='main-container'>
-            <video autoPlay loop muted playsInline>
-            <source src={backgroundVideo} type='video/mp4' alt='Video by 33studiodesign from Pixabay'/>
-            </video>
             <div className='responsive-type-animation'>
             <TypeAnimation className='name' sequence={["Home Share Application", 1000]} speed={20} style={{ fontSize: '12vmin', fontFamily: 'Tahoma', textShadow: '3px 2px #5b05f9', color: '#fff'}}/>
             </div> 
