66c299bfd834d74e4e31fe9c12cf947c118c5f86
Sierra Williams <78832175+sierrawillliams@users.noreply.github.com>
2025-02-04 14:03:16 -0800

    trying to get pdf to render

--- src/components/pages/OMR.js
+++ src/components/pages/OMR.js
@@ -7,7 +7,7 @@ const OMR = () => {
     return (
         <div className="main" style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <iframe 
-                src="/omr.pdf"
+                src="https://okincbucket.s3.us-east-1.amazonaws.com/Outcomes+Management+Report+2023+update.pdf"
                 style={{ width: '80%', height: '90%', border: 'none', paddingTop: '80px'}}
                 title="OMR PDF"
             />
