7291060ea6d1f1c52e2928a96585218019ab11e0
Sierra Williams <78832175+sierrawillliams@users.noreply.github.com>
2025-02-04 13:48:37 -0800

    Update OMR.js

--- src/components/pages/OMR.js
+++ src/components/pages/OMR.js
@@ -9,10 +9,6 @@ const OMR = () => {
                 style={{ width: '80%', height: '90%', border: 'none', paddingTop: '80px'}}
                 title="OMR PDF"
             />
-            <embed src="/omr.pdf" width="100%" height="600px" type="application/pdf" />
-            <object data="/omr.pdf" width="100%" height="600px" type="application/pdf">
-                <p>Your browser doesn’t support PDFs. Please download the PDF to view it: <a href="/your-pdf-url.pdf">Download PDF</a>.</p>
-            </object>
         </div>
     );
 };
