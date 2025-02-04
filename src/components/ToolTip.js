0b10df523d9193e3bc79e9184e1f58e9f3191432
Sierra Williams <78832175+sierrawillliams@users.noreply.github.com>
2025-02-04 14:44:33 -0800

    pdf fix?

--- src/components/ToolTip.js
+++ src/components/ToolTip.js
@@ -74,11 +74,12 @@ const ToolTip = () => {
             <img className='seal' src={CARFSeal}/>
         </div>
         <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <iframe 
-                src="https://okincbucket.s3.us-east-1.amazonaws.com/Outcomes%20Management%20Report%202023%20update.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAXAECKAJWLV3L4BFN%2F20250204%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250204T221843Z&X-Amz-Expires=300&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEB8aCXVzLWVhc3QtMSJHMEUCIDdz9vonHF%2FML6IeyP4%2Frntk%2FkZl5dZE1u6jseX7P9qGAiEAqd74G%2B3kWAlT9V9VysxE%2F62ghqld%2Bm9i4itneyNMfCwq7AIINxAAGgw0ODEzMDk2MjI4OTIiDKmPI%2FsHlgYEYiwqUCrJAuCnuuf4ECqMa0KeGFqo9IHJwa5UHkimRQjh2YOrgxjFshEaSz6Nk%2FLkSbIcBg2EYeJjNTSVyeztIaE4bI9JtOV%2FY7bq74%2BF3FgePHkq7YLcQsYNLb2pe%2Bz1NMLpVmkcjzIrg9gm979t4jgKpGfRf2mHTGYeCOLlqP2I24XzentSGr3fyMK94myWbvnHwG2SQ3BLGpETD1e6%2BZeHH4CFoFExXQgTrHVyd3IczvF8%2BmlZMRFKkT9djkfA5DcPjHjKf0ijJazTfzKoxA9BlAqYTJjLLD1qGluhhxrPdLtwSztn7%2BukYPVJp4TjV7K4h00YA%2BqX3J0F3OYHnpN%2Ffj006T5kbSQEPmJxFwIWFpjGsgcjDD9tPUtFnnUIaj5%2BnWlqRt6bj2cLUzFm3iFtgfTPJ46I6U4tGOUxwg%2BXncGJKOxah1NkN%2FXSanegMOGSir0GOrMC5WtyJKQVDN22rCMEXI2XnraZA47yJHfKMrAACHTGKWNQQOu%2BOol1%2F%2FNY4QHnny%2BnBfemiY1UjGamiV6SYqQ4%2Fns6B%2BZrPcldawZAxCbD7Mwlk1Mu32iQr%2FviI1Z1HCy8SYisi7wxOCT2roF1irgWP9NPVxPwicJlKCMDHVOd1NOhcwOC9coOA%2FDIOLCuPO3CbBG7H0IK3hb9YWBvxxtqgGPfwUO4Aj%2BiOFvB6WHXXbLILF%2FYxIeeehXhkHKV%2FywOFC5nsl%2FNgz9SsldoeBxHAEjTaE9JM32%2BRYQMKF%2FWt7d%2BYKk6M7sBRs4dTqNHMOWWcCpUrXPjeBpKlu9n616vQMy3XeNaxd5DB4UDKNfrdMNWe8KLLjCxa1thlB09ArzAZeqH75zNkn%2BRccWxHN6bDWzwvQ%3D%3D&X-Amz-Signature=edf5b449d818073b4f99b20a93422417ce77d93ae6aba6c76d702faf2d308909&X-Amz-SignedHeaders=host&response-content-disposition=inline"
-                style={{ width: '80%', height: '90%', border: 'none'}}
-                title="OMR PDF"
-            />
+        <iframe 
+  src="https://okincbucket.s3.us-east-1.amazonaws.com/AccreditationReport"
+  style={{ width: '80%', height: '90%', border: 'none'}}
+  title="OMR PDF"
+/>
+
         </div>
     </div>
   );
