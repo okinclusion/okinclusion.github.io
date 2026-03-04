4c62675403bdc0ed75e2bfd5875de2e0562d4fe7
Sierra Williams <78832175+sierrawillliams@users.noreply.github.com>
2026-03-04 14:48:29 -0800

    New design and layout for the home-page. Modern and sleek

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/pages/Home';
+import Home from './components/pages/Home.jsx';
 import History from './components/pages/History';
 import Mission from './components/pages/Mission';
 import Team from './components/pages/Team';
