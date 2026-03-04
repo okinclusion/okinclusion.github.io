01b10328a9dfd3e456be7e131067a185da2a2d58
Sierra Williams <78832175+sierrawillliams@users.noreply.github.com>
2026-03-04 08:21:12 -0800

    Testing header image fix

--- src/components/HomeShareApply.js
+++ src/components/HomeShareApply.js
@@ -7,7 +7,7 @@ function Apply() {
 
   return (
     <>
-        <div className='main-container'>
+        <div className='header-container'>
             <div className='responsive-type-animation'>
             <TypeAnimation className='name' sequence={["Home Share Application", 1000]} speed={20} style={{ fontSize: '12vmin', fontFamily: 'Tahoma', textShadow: '3px 2px #5b05f9', color: '#fff'}}/>
             </div> 
